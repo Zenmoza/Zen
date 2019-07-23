15d37ab3859d76add278649bb3cbbb4f296bae28
Zenmoza <34386842+Zenmoza@users.noreply.github.com>
2019-07-23 13:00:50 +0700

    Create fakyu.js

---
+++ fakyu.js
@@ -0,0 +1,61 @@
+<script>
+        // membuat objek elemen
+        var hasil = document.getElementById("hasil-output");
+
+        // menampilkan output ke elemen hasil
+        hasil.innerHTML = '<!--more-->
+<div id='--fixedsize'>
+<div id='targetzx' class="slider fade">
+<!-- batas -->
+
+<div>
+<img border="0"  src="https://cdn.idntimes.com/content-images/post/20190108/gmbr-01-8f33115922cbb20cf150de6b519d3a15-f6ec8bb5b30d6bc4f07349cb1a4bbe2b_600x400.jpg" />
+</div>
+
+<div>
+<img border="0"  src="https://cdn.idntimes.com/content-images/post/20190108/gmbr-01-8f33115922cbb20cf150de6b519d3a15-f6ec8bb5b30d6bc4f07349cb1a4bbe2b_600x400.jpg" />
+</div>
+
+<div>
+<img border="0"  src="https://cdn.idntimes.com/content-images/post/20190108/gmbr-01-8f33115922cbb20cf150de6b519d3a15-f6ec8bb5b30d6bc4f07349cb1a4bbe2b_600x400.jpg" />
+</div>
+
+<div>
+<img border="0"  src="https://cdn.idntimes.com/content-images/post/20190108/gmbr-01-8f33115922cbb20cf150de6b519d3a15-f6ec8bb5b30d6bc4f07349cb1a4bbe2b_600x400.jpg" />
+</div>
+
+<div>
+<img border="0"  src="https://cdn.idntimes.com/content-images/post/20190108/gmbr-01-8f33115922cbb20cf150de6b519d3a15-f6ec8bb5b30d6bc4f07349cb1a4bbe2b_600x400.jpg" />
+</div>
+
+<div>
+<img border="0"  src="https://cdn.idntimes.com/content-images/post/20190108/gmbr-01-8f33115922cbb20cf150de6b519d3a15-f6ec8bb5b30d6bc4f07349cb1a4bbe2b_600x400.jpg" />
+</div>
+
+<div>
+<img border="0"  src="https://cdn.idntimes.com/content-images/post/20190108/gmbr-01-8f33115922cbb20cf150de6b519d3a15-f6ec8bb5b30d6bc4f07349cb1a4bbe2b_600x400.jpg" />
+</div>
+
+<div>
+<img border="0"  src="https://cdn.idntimes.com/content-images/post/20190108/gmbr-01-8f33115922cbb20cf150de6b519d3a15-f6ec8bb5b30d6bc4f07349cb1a4bbe2b_600x400.jpg" />
+</div>
+
+<div>
+<img border="0"  src="https://cdn.idntimes.com/content-images/post/20190108/gmbr-01-8f33115922cbb20cf150de6b519d3a15-f6ec8bb5b30d6bc4f07349cb1a4bbe2b_600x400.jpg" />
+</div>
+
+<div>
+<img border="0"  src="https://cdn.idntimes.com/content-images/post/20190108/gmbr-01-8f33115922cbb20cf150de6b519d3a15-f6ec8bb5b30d6bc4f07349cb1a4bbe2b_600x400.jpg" />
+</div>
+
+<div>
+<img border="0"  src="https://cdn.idntimes.com/content-images/post/20190108/gmbr-01-8f33115922cbb20cf150de6b519d3a15-f6ec8bb5b30d6bc4f07349cb1a4bbe2b_600x400.jpg" />
+</div>
+
+<div>
+<img border="0"  src="https://cdn.idntimes.com/content-images/post/20190108/gmbr-01-8f33115922cbb20cf150de6b519d3a15-f6ec8bb5b30d6bc4f07349cb1a4bbe2b_600x400.jpg" />
+</div>
+
+<!-- batas -->
+</div></div>";
+    </script>
